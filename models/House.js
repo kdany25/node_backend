cef54affdf4fc5ecfff07ee8c805e0fb5621d98e
kdany25 <kabadany25@gmail.com>
2022-07-04 21:23:22 +0200

    updated codes

--- models/House.js
+++ models/House.js
@@ -5,7 +5,6 @@ const HouseSchema = new mongoose.Schema(
     owner: { type: String, required: true },
     address: { type: String, required: true },
     phone: { type: String, required: true },
-    email: { type: String, required: true }, 
     cost: { type: String, required: true },
   
     
--- models/User.js
+++ models/User.js
@@ -1,19 +1,19 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema(
-  {
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    phone: { type: String, required: true },
-    gender: {
-      type: String,
-      enum: ["MALE", "FEMALE"],
-      required: true,
-    },
-  },
-  { timestamps: true }
+	{
+		firstName: { type: String},
+		lastName: { type: String},
+		email: { type: String, required: true, unique: true },
+		password: { type: String, required: true },
+		phone: { type: String, required: true },
+		gender: {
+			type: String,
+			enum: ["MALE", "FEMALE"],
+			// required: true,
+		},
+	},
+	{ timestamps: true }
 );
 
 module.exports = mongoose.model("User", UserSchema);
