656af3858a18ca3c4ba32e417334b1bc4051a94b
kdany25 <kabadany25@gmail.com>
2023-01-26 10:35:58 +0200

    feat: cleaned codes

--- models/FeedBack.js
+++ models/FeedBack.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 const FeedBackSchema = new mongoose.Schema(
-  {
-    data: { type: String, required: true },
-    title: { type: String, required: true },
-    entry_id: { type: String, required: true, unique: true },
-  },
-  { timestamps: true }
+	{
+		data: { type: String, required: true },
+		title: { type: String, required: true },
+		entry_id: { type: String, required: true, unique: true },
+	},
+	{ timestamps: true }
 );
 
 module.exports = mongoose.model("FeedBack", FeedBackSchema);
--- models/Health.js
+++ models/Health.js
@@ -1,18 +1,18 @@
 import mongoose from "mongoose";
 
 const HealthSchema = new mongoose.Schema(
-  {
-    pname: { type: String, required: true },
-    email: { type: String},
-    phone: { type: String, required: true },
-    age : { type: String, required: true,},
-    desease: { type: String, required: true,},
-    address : { type: String, required: true,},
-    hospitalised: { type: Boolean, default: true },
-    cost : { type: String, required: true,},
-    },
-  
-  { timestamps: true }
+	{
+		pname: { type: String, required: true },
+		email: { type: String },
+		phone: { type: String, required: true },
+		age: { type: String, required: true },
+		desease: { type: String, required: true },
+		address: { type: String, required: true },
+		hospitalised: { type: Boolean, default: true },
+		cost: { type: String, required: true },
+	},
+
+	{ timestamps: true }
 );
 
-module.exports = mongoose.model("Health", HealthSchema );
+module.exports = mongoose.model("Health", HealthSchema);
