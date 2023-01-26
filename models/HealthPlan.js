87ad75e0849f0d2c4ce1b05e7dceee77acb9ca81
kdany25 <kabadany25@gmail.com>
2023-01-26 10:39:02 +0200

    feat: cleaned codes

--- models/HealthPlan.js
+++ models/HealthPlan.js
@@ -1,18 +1,17 @@
 import mongoose from "mongoose";
 
 const HealthPlanSchema = new mongoose.Schema(
-  {
-    
-    location: { type: String, required: true },
-    number: { type: Number, required: true },
-    desease: { type: String, required: true,},
-    period : { type: String, required: true,},
-    doctor : { type: String, required: true,},
-    cost : { type: String, required: true,},
-    priority: { type: Number, required: true },
-    },
-  
-  { timestamps: true }
+	{
+		location: { type: String, required: true },
+		number: { type: Number, required: true },
+		desease: { type: String, required: true },
+		period: { type: String, required: true },
+		doctor: { type: String, required: true },
+		cost: { type: String, required: true },
+		priority: { type: Number, required: true },
+	},
+
+	{ timestamps: true }
 );
 
-module.exports = mongoose.model("HealthPlan", HealthPlanSchema );
+module.exports = mongoose.model("HealthPlan", HealthPlanSchema);
--- models/House.js
+++ models/House.js
@@ -1,15 +1,13 @@
 import mongoose from "mongoose";
 
 const HouseSchema = new mongoose.Schema(
-  {
-    owner: { type: String, required: true },
-    address: { type: String, required: true },
-    phone: { type: String, required: true },
-    cost: { type: String, required: true },
-  
-    
-  },
-  { timestamps: true }
+	{
+		owner: { type: String, required: true },
+		address: { type: String, required: true },
+		phone: { type: String, required: true },
+		cost: { type: String, required: true },
+	},
+	{ timestamps: true }
 );
 
 module.exports = mongoose.model("House", HouseSchema);
